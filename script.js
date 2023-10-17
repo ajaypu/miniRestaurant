8e29d76a50459f49804644c8301e8e09d51f2bbc
Ajay Prakash <21376002@pondiuni.ac.in>
2023-10-17 18:19:55 +0530

    DRY Code & Delete Functionlity Added

--- script.js
+++ script.js
@@ -2,90 +2,70 @@ const form = document.querySelector(".form");
 const price = document.querySelector("#price");
 const dish = document.querySelector("#dish");
 const table = document.querySelector("#table");
-
 const table1List = document.querySelector("#table-1-list");
 const table2List = document.querySelector("#table-2-list");
 const table3List = document.querySelector("#table-3-list");
+let axiosAPI =
+  "https://crudcrud.com/api/3fbd1d9536c2440ab59e829da2ed686e/miniRestaurantApp";
 
-const axiosAPI =
-  "https://crudcrud.com/api/c7bd9b52c53943fea0f8764b5c63a884/miniRestaurantApp";
-
-//form eventListener
-form.addEventListener("submit", onSubmit);
-
-// delete button
-function deleteButton(obj) {
-  const delBtn = document.createElement("button");
+// creating Delete button
+function deleteBtn(obj) {
+  let delBtn = document.createElement("button");
   delBtn.className = "del-btn";
-  delBtn.appendChild(document.createTextNode("Delete Order"));
+  delBtn.appendChild(document.createTextNode("delete Order"));
   // delete button eventListener
-  delBtn.addEventListener("click", delItem(obj, e));
-  async function delItem(obj, e) {
-    let tableDetails = await axios.get(axiosAPI);
-    let tableDe = tableDetails.data.find((t) => {
-      return t.price === obj.price && t.dish === obj.dish;
+  delBtn.addEventListener("click", (e) => delOrder(obj, e));
+  async function delOrder(obj, e) {
+    let tableItems = await axios.get(axiosAPI);
+    let orderItems = tableItems.data.find((io) => {
+      return io.price === obj.price && io.dish === obj.dish;
     });
+    await axios.delete(`${axiosAPI}/${orderItems._id}`);
+    const li = e.target.parentElement;
+    if (obj.table === "table1") {
+      table1List.removeChild(li);
+    } else if (obj.table === "table2") {
+      table2List.removeChild(li);
+    } else if (obj.table === "table3") {
+      table3List.removeChild(li);
+    }
   }
+  //
   return delBtn;
 }
-// creating li element
-function li1(obj) {
+
+// creating li
+function li(obj) {
   const li = document.createElement("li");
-  li.className = "table-li";
+  li.className = "li-item";
+  li.appendChild(deleteBtn(obj));
   li.appendChild(document.createTextNode(`${obj.price} ${obj.dish}`));
-  li.appendChild(deleteButton());
-  table1List.appendChild(li);
+  if (obj.table === "table1") {
+    table1List.appendChild(li);
+  } else if (obj.table === "table2") {
+    table2List.appendChild(li);
+  } else if (obj.table === "table3") {
+    table3List.appendChild(li);
+  }
 }
-//
-function li2(obj) {
-  const li = document.createElement("li");
-  li.className = "table-li";
-  li.appendChild(document.createTextNode(`${obj.price} ${obj.dish}`));
-  li.appendChild(deleteButton());
-  table2List.appendChild(li);
-}
-//
-function li3(obj) {
-  const li = document.createElement("li");
-  li.className = "table-li";
-  li.appendChild(document.createTextNode(`${obj.price} ${obj.dish}`));
-  li.appendChild(deleteButton());
-  table3List.appendChild(li);
-}
-async function onSubmit(e) {
+form.addEventListener("submit", onSubmit);
+function onSubmit(e) {
   e.preventDefault();
+
   let obj = {
     price: price.value,
     dish: dish.value,
     table: table.value,
   };
-
-  if (table.value === "table 1") {
-    li1();
-  } else if (table.value === "table 2") {
-    li2();
-  } else if (table.value === "table 3") {
-    li3();
-  }
-  await axios.post(axiosAPI, obj);
+  li(obj);
+  axios.post(axiosAPI, obj);
 }
+window.addEventListener("DOMContentLoaded", tableItemsOnScreen);
 
-window.addEventListener("DOMContentLoaded", AddNewUshersOnScreen);
-
-function AddNewUshersOnScreen() {
-  axios.get(axiosAPI).then((res) => {
-    for (let i = 0; i < res.data.length; i++) {
-      li1(res.data[i]);
-    }
-  });
-  axios.get(axiosAPI).then((res) => {
-    for (let i = 0; i < res.data.length; i++) {
-      li2(res.data[i]);
-    }
-  });
-  axios.get(axiosAPI).then((res) => {
-    for (let i = 0; i < res.data.length; i++) {
-      li3(res.data[i]);
-    }
-  });
+async function tableItemsOnScreen(e) {
+  //
+  const tableItems = await axios.get(axiosAPI);
+  for (let i = 0; i < tableItems.data.length; i++) {
+    li(tableItems.data[i]);
+  }
 }
